0ff4a270244d39803d6cb6732965ae31477066cb
Nikolai Onken <nikolai@c9.io>
2014-08-12 10:29:47 +0000

    Add tests and remove unclear matchers

--- message_matchers.js
+++ message_matchers.js
@@ -15,26 +15,23 @@ define(function(require, exports, module) {
         var messages = {
             generic: {
                 wrongPortIP: wrongPortIP + "Try passing $PORT and $IP to properly launch your application.",
-                appRunning: "Your code is running at \u001B[04;36m" + "https://" + c9.hostname
+                appRunning: "Your code is running at \u001B[04;36m" + "https://" + c9.hostname,
+                bindToInternalIP: wrongPortIP + "Only binding to the internal IP configured in $IP is supported."
             },
             rails: {
                 wrongPortIP: wrongPortIP + "For rails, use: 'rails s -p $PORT -b $IP'\r\n" +
                     "For Sinatra, use: ruby app.rb -p $PORT -o $IP'"
+            },
+            node: {
+                wrongPortIP: wrongPortIP + "Node: use 'process.env.PORT' as the port and 'process.env.IP' as the host in your scripts."
+            },
+            django: {
+                wrongPortIP: wrongPortIP + "use './manage.py runserver $IP:$PORT' to run your Django application."
             }
             
         };
         
         var matchers = [
-            {
-                // Ionic, Meteor wrong port
-                pattern: /(Running dev server:|App running at:).*?:(?=\d{4})(?!8080)/,
-                message: messages.generic.wrongPortIP
-            },
-            {
-                // Ionic, Meteor correct port
-                pattern: /(Running dev server:|App running at:).*?:(?=8080)/,
-                message: messages.generic.appRunning
-            },
             {
                 // Generic
                 pattern: /server(?: is | )(?:listening|running) (?:at|on).*?(?=\d{4})(?!8080)/i,
@@ -43,6 +40,16 @@ define(function(require, exports, module) {
             {
                 // Generic correct port
                 pattern: /server(?: is | )(?:listening|running) (?:at|on).*?(?=8080)/i,
+                message: messages.generic.appRunning
+            },
+            {
+                // Ionic, Meteor wrong port
+                pattern: /(Running dev server:|App running at:).*?:(?=\d{4})(?!8080)/,
+                message: messages.generic.wrongPortIP
+            },
+            {
+                // Ionic, Meteor correct port
+                pattern: /(Running dev server:|App running at:).*?:(?=8080)/,
                 message: messages.generic.appRunning
             },
             {
@@ -58,32 +65,17 @@ define(function(require, exports, module) {
             {
                 // Node app
                 pattern: /Error: listen (?:EADDRINUSE|EACCES|EADDRNOTAVAIL)/,
-                message: wrongPortIP + "Node: use 'process.env.PORT' as the port and 'process.env.IP' as the host in your scripts."
+                message: messages.node.wrongPortIP
             },
             {
                 // Django app
                 pattern: /Error: You don't have permission to access that port./,
-                message: wrongPortIP + "use './manage.py runserver $IP:$PORT' to run your Django application."
-            },
-            {
-                // Tunneling to some database provider
-                pattern: /Errno: EACCES Permission Denied - bind\(2\)/,
-                message: wrongPortIP + "Only binding to the internal IP configured in $IP is supported."
-            },
-            {
-                // MongoDB
-                pattern: /\[initandlisten\] ERROR: listen\(\): bind\(\) failed errno:13 Permission denied for socket: 0\.0\.0\.0:/,
-                message: wrongPortIP + "Please bind to the internal IP using --bind_ip=$IP"
-            },
-            {
-                // Meteor/generic
-                pattern: /Can't listen on port /,
-                message: wrongPortIP + "Please bind to IP $IP and port $PORT." 
+                message: messages.django.wrongPortIP
             },
             {
                 // FALLBACK/GENERIC
-                pattern: /Permission denied for socket: 0\.0\.0\.0:/,
-                message: wrongPortIP + "Only binding to the internal IP configured in $IP is supported."
+                pattern: /Permission denied for socket: 0\.0\.0\.0/,
+                message: messages.generic.bindToInternalIP
             }
         ];
         return {
--- monitor_test.js
+++ monitor_test.js
@@ -34,25 +34,83 @@ require([
             formatMessageSpy = sinon.spy(messageHandler, "formatMessage");
             
         });
-        it("should translate `App running at: http://0.0.0.0:8080/`", function() {
-            messageHandler.handleMessage("App running at: http://0.0.0.0:8080/");
-            expect(formatMessageSpy.calledWith(messages.generic.appRunning)).to.equal(true)
+        it("catches generic (listening at) wrong port", function() {
+            messageHandler.handleMessage("Server listening at http://localhost:3000/");
+            expect(formatMessageSpy.calledWith(messages.generic.wrongPortIP)).to.equal(true);
         });
-        it("should translate `App running at: http://0.0.0.0:8081/`", function() {
-            messageHandler.handleMessage("App running at: http://0.0.0.0:8081/");
-            expect(formatMessageSpy.calledWith(messages.generic.wrongPortIP)).to.equal(true)
+        it("catches generic (listening at) running", function() {
+            messageHandler.handleMessage("Server listening at http://localhost:8080/");
+            expect(formatMessageSpy.calledWith(messages.generic.appRunning)).to.equal(true);
         });
-        it("should translate `Running dev server: http://0.0.0.0:8080`", function() {
-            messageHandler.handleMessage("Server running on http://0.0.0.0:8080");
-            expect(formatMessageSpy.calledWith(messages.generic.appRunning)).to.equal(true)
+        it("catches generic (is listening at) wrong port", function() {
+            messageHandler.handleMessage("Server is listening at http://localhost:3000/");
+            expect(formatMessageSpy.calledWith(messages.generic.wrongPortIP)).to.equal(true);
         });
-        it("should translate `Running dev server: http://0.0.0.0:8081`", function() {
-            messageHandler.handleMessage("Server running on http://0.0.0.0:8081");
-            expect(formatMessageSpy.calledWith(messages.generic.wrongPortIP)).to.equal(true)
+        it("catches generic (is listening at) running", function() {
+            messageHandler.handleMessage("Server is listening at http://localhost:8080/");
+            expect(formatMessageSpy.calledWith(messages.generic.appRunning)).to.equal(true);
         });
-        it("should translate `Running dev server: http://0.0.0.0:8081`", function() {
-            messageHandler.handleMessage("Server running on http://0.0.0.0:8081");
-            expect(formatMessageSpy.calledWith(messages.generic.wrongPortIP)).to.equal(true)
+        it("catches generic (is running on) wrong port", function() {
+            messageHandler.handleMessage("Server is running on http://localhost:3000/");
+            expect(formatMessageSpy.calledWith(messages.generic.wrongPortIP)).to.equal(true);
+        });
+        it("catches generic (is running on) running", function() {
+            messageHandler.handleMessage("Server is running on http://localhost:8080/");
+            expect(formatMessageSpy.calledWith(messages.generic.appRunning)).to.equal(true);
+        });
+        it("catches ionic wrong port", function() {
+            messageHandler.handleMessage("Running dev server: http://0.0.0.0:8081/");
+            expect(formatMessageSpy.calledWith(messages.generic.wrongPortIP)).to.equal(true);
+        });
+        it("catches ionic running", function() {
+            messageHandler.handleMessage("Running dev server: http://0.0.0.0:8080/");
+            expect(formatMessageSpy.calledWith(messages.generic.appRunning)).to.equal(true);
+        });
+        it("catches meteor wrong port", function() {
+            messageHandler.handleMessage("App running at: http://localhost:3000/");
+            expect(formatMessageSpy.calledWith(messages.generic.wrongPortIP)).to.equal(true);
+        });
+        it("catches meteor running", function() {
+            messageHandler.handleMessage("App running at: http://localhost:8080/");
+            expect(formatMessageSpy.calledWith(messages.generic.appRunning)).to.equal(true);
+        });
+        it("catches Webrick running", function() {
+            messageHandler.handleMessage("mostafaeweda@demo-project\r\n\
+                INFO  WEBrick::HTTPServer#start: pid=5462 port=8080");
+            expect(formatMessageSpy.calledWith(messages.generic.appRunning)).to.equal(true);
+        });
+        it("catches rails/sinatra address in use error", function() {
+            messageHandler.handleMessage("WARN  TCPServer Error: Address already in use - bind(2)");
+            expect(formatMessageSpy.calledWith(messages.rails.wrongPortIP)).to.equal(true);
+        });
+        it("catches node address in use error", function() {
+            messageHandler.handleMessage("events.js:48\n\
+                    throw arguments[1]; // Unhandled 'error' event\n\
+                    Error: listen EADDRINUSE\n\
+                    at errnoException (net.js:670:11)\n\
+                    at Array.0 (net.js:771:26)\n\
+                    at EventEmitter._tickCallback (node.js:190:38)\n");
+            expect(formatMessageSpy.calledWith(messages.node.wrongPortIP)).to.equal(true);
+        });
+    
+        it("catches node permission error", function() {
+            messageHandler.handleMessage("events.js:48\n\
+                    throw arguments[1]; // Unhandled 'error' event\n\
+                    Error: listen EACCESS\n\
+                    at errnoException (net.js:670:11)\n\
+                    at Array.0 (net.js:771:26)\n\
+                    at EventEmitter._tickCallback (node.js:190:38)\n");
+            expect(formatMessageSpy.calledWith(messages.node.wrongPortIP)).to.equal(true);
+        });
+        
+        it("catches django error", function () {
+            messageHandler.handleMessage("Error: You don't have permission to access that port.\n");
+            expect(formatMessageSpy.calledWith(messages.django.wrongPortIP)).to.equal(true);
+        });
+        
+        it("catches permission denied for socket", function() {
+            messageHandler.handleMessage("Permission denied for socket: 0.0.0.0\n");
+            expect(formatMessageSpy.calledWith(messages.generic.bindToInternalIP)).to.equal(true);
         });
     });
         
