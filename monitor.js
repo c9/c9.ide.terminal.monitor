5dfa18ff717e4ed8fb507ba54359a9c7218f9fe9
Nikolai Onken <nikolai@c9.io>
2014-08-20 07:16:06 +0000

    Handle 'no newline event'

--- monitor.js
+++ monitor.js
@@ -33,26 +33,39 @@ define(function(require, exports, module) {
             var seenUpTo = 0;
             var hasResizeCompleted = false;
             
+            function handleMessage(message) {
+                var tab = session.tab;
+                if (tab.isActive() && tabManager.focussedTab === tab) {
+                    var referenceNode = tab.aml.$pHtmlNode.querySelector('.session_page.curpage');
+                    messageHandler.handleMessage(message, referenceNode);
+                }
+            }
+            
             terminal.on("newline", function(e) {
                 var y = e.y;
                 var linesIndex = y + e.ybase - 1;
                 var line = e.lines[linesIndex].map(function(character) { return character[1]; }).join("");
-               
+                
                 if (!hasResizeCompleted) {
                     if (line.length) {
                         seenUpTo = e.y;
                     }
                     return;
                 }
+                
+                // There are cases where newline doesn't fire for a "rendered" newline.
+                // Making sure that we check lines when we encounter these gaps.
+                while (seenUpTo < y) {
+                    seenUpTo++;
+                    var tmpLinesIndex = seenUpTo + e.ybase - 1;
+                    var tmpLine = e.lines[tmpLinesIndex].map(function(character) { return character[1]; }).join("");
+                    handleMessage(tmpLine);
+                }
 
                 if (y - 1 > seenUpTo) return;
                 seenUpTo = y;
                 
-                var tab = session.tab;
-                if (tab.isActive() && tabManager.focussedTab === tab) {
-                    var referenceNode = tab.aml.$pHtmlNode.querySelector('.session_page.curpage');
-                    messageHandler.handleMessage(line, referenceNode);
-                }
+                handleMessage(line);
             });
             
             var resizeTimeout;
